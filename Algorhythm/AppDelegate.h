09ecdfa28555b44a61ca8d005f843f4556d47713
Matthew D'Amore <msd88@drexel.edu>
2015-06-18 18:33:07 -0400

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Matthew D'Amore on 6/18/15.
+//  Copyright (c) 2015 Matthew D'Amore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
